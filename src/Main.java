2d0ac4b1d929522d12a7b1083b9f70557ae212d5
amirah763 <amirahnb27@gmail.com>
2026-04-14 09:40:39 -0700

    Added the add assignment logic

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,36 @@ public class Main{
         boolean running = true;
 
         while(running){
-            
+            System.out.println("Study Planner");
+            System.out.println("1. Add Assignment");
+            System.out.println("2. View Assignments");
+            System.out.println("3. Exit");
+
+            System.out.println("Choose an option: ");
+            int choice = input.nextInt();
+            input.nextLine();
+
+            if (choice == 1);{
+                System.out.println("Enter class name: ");
+                String className = input.nextLine();
+
+                System.out.println("Enter assignment name: ");
+                String name = input.nextLine();
+
+                System.out.println("Enter due date (Ex: April 14): ");
+                String dueDate = input.nextLine();
+
+                Assignment a = new Assignment(name, className, dueDate);
+                planner.addAssignment(a);
+
+                System.out.println("Assignment added");
+            }
+
+            if(choice == 2){
+                planner.displayAllAssignments();
+            }
+
         }
     }
 
 }
-
