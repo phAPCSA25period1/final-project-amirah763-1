eaba460b296a57f7b35af621ea916c182b9ac393
amirah763 <amirahnb27@gmail.com>
2026-04-22 09:37:19 -0700

    Create Task class for managing tasks

---
+++ src/Task.java
@@ -0,0 +1,31 @@
+public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description){
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isCompleted(){
+        return completed;
+    }
+
+    public void markComplete(){
+        completed = true;
+    }
+
+    public String toString(){
+        if (completed){
+            return "complete! " + description;
+        }
+        else{
+            return "incomeplete " + description;
+        }
+    }
+
+}
