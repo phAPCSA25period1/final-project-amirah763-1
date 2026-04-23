ef3eb71ceac024c8844de5dde23c79f9a2120783
amirah763 <amirahnb27@gmail.com>
2026-04-23 09:40:40 -0700

    Add Task methods into Assignment class
    Added task management functionality to Assignment class.

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,14 +1,18 @@
+public class Assignment {import java.util.ArrayList;
+
 public class Assignment {
     private String name;
     private String className;
     private String dueDate;
     private boolean completed;
+    private ArrayList<Task>tasks;
 
     public Assignment(String name, String className, String dueDate){
         this.name = name;
         this.className = className;
         this.dueDate = dueDate;
         this.completed = false;
+        tasks = new ArrayList<>();
     }
 
     public String getName(){
@@ -36,10 +40,30 @@ public class Assignment {
     }
 
     public boolean isComplete(){
-        return completed; 
+        return completed;
     }
 
+    public void addTask(Task task) {
+        tasks.add(task);
+    }
+
+    public void displayTasks(){
+        if (tasks.size() == 0){
+            System.out.println("No tasks yet");
+        }
+        for (int i = 0; i < tasks.size(); i++){
+            System.out.println((i +1) + ". " + tasks.get(i));
+        }
+    }
+
+    public void markTaskComplete(int index){
+        if (index >= 0 && index < tasks.size()){
+            tasks.get(index).markComplete();
+        }
+    }
+
+
     public String toString(){
-        return className + ": " + name + "(due: " + dueDate + ")";
+        return className + ": " + name + " " + "(due: " + dueDate + ")";
     }
 }
