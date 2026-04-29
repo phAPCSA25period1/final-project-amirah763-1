74a6f0ddd0d59a5c39b0d66374052836c92e4c7c
amirah763 <amirahnb27@gmail.com>
2026-04-29 09:39:17 -0700

    Add schedule management to Planner class
    Added a schedule array to store assignments and methods to assign and display the schedule.

--- src/Planner.java
+++ src/Planner.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class Planner{
     private ArrayList<Assignment> assignments;
+    private Assignment [][] schedule;
 
     public Planner(){
         assignments = new ArrayList<>();
+        schedule = new Assignment [7][3];
     }
 
     public void addAssignment(Assignment a){
@@ -26,4 +28,24 @@ public class Planner{
         return assignments;
     }
 
+    public void assignToSchedule(int day, int period, Assignment a){
+        schedule[day][period] = a;
+    }
+
+    public void displaySchedule(){
+        String[] days ={"Mon", "Tues", "Wed", "Thu", "Fri", "Sat", "Sun"};
+        for(int i = 0; i < schedule.length; i++){
+            System.out.println(days[i] + ": ");
+
+            for (int j = 0; j < schedule[i].length; j++){
+                if (schedule[i][j] != null){
+                    System.out.println(schedule[i][j].getName() + " | ");
+                }
+                else{
+                    System.out.println("Empty | ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }
