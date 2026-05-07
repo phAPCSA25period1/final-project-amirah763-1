7ad04661ff0667fb1b253bfa576a1f329ec0433d
amirah763 <amirahnb27@gmail.com>
2026-05-07 15:24:21 -0700

    Add JavaDoc comments to Task class
    Added JavaDoc comments to Task class methods and constructor.

--- src/Task.java
+++ src/Task.java
@@ -1,29 +1,60 @@
+/**
+ * Represents a task that can be marked as complete or incomplete.
+ * Each task has a description and a completion status.
+ */
 public class Task {
+    /** The description of the task */
     private String description;
+    /** Whether the task has been marked as complete */
     private boolean completed;
 
-    public Task(String description){
+    /**
+     * Constructs a Task with the given description.
+     * The task is initially marked as incomplete.
+     *
+     * @param description the description of the task
+     */
+    public Task(String description) {
         this.description = description;
         this.completed = false;
     }
 
-    public String getDescription(){
+    /**
+     * Returns the description of this task.
+     *
+     * @return the task description
+     */
+    public String getDescription() {
         return description;
     }
 
-    public boolean isCompleted(){
+    /**
+     * Returns whether this task has been marked as complete.
+     *
+     * @return true if the task is complete, false otherwise
+     */
+    public boolean isCompleted() {
         return completed;
     }
 
-    public void markComplete(){
+    /**
+     * Marks this task as complete.
+     */
+    public void markComplete() {
         completed = true;
     }
 
-    public String toString(){
-        if (completed){
+    /**
+     * Returns a string representation of this task.
+     * Shows the completion status with a checkmark or X, followed by the
+     * description.
+     *
+     * @return a formatted string with status and description
+     */
+    public String toString() {
+        if (completed) {
             return "[complete ✓] " + description;
-        }
-        else{
+        } else {
             return "[incomplete ✗] " + description;
         }
     }
